83d7a248c75642c324ffb40ba637e6be3bb52295
Tareq Malas <tareq.malas@kaust.edu.sa>
2015-01-08 12:09:49 +0100

    added intial scripts for the Phi

---
+++ scripts/mic/run_test.sh
@@ -0,0 +1,24 @@
+run_test() {
+
+
+  t=$1
+  aff=$2
+  group=$3
+  N=$4
+  ts=$5
+  k=$6
+  nt=$7
+  tgs=$8
+  cs=$9
+  mwdt=$10
+
+  res_file="${res_dir}kernel${k}_tgs${tgs}_ts${ts}_th${t}_grp_${group}_affinity${aff}_mwdtype${mwdt}.txt"
+
+  thx=`expr $t - 1`
+
+  echo export OMP_NUM_THREADS=$t ';'likwid-perfctr -m -C $aff -g $group -- /home/hpc/unrz/unrz280/girih/build_dp/mwd_kernel --n-tests 4 --nx $N --ny $N --nz $N --target-ts $ts --target-kernel $k --nt $nt --use-omp-stat-sched --thread-group-size $tgs --cache-size $cs --mwd-type $mwdt #| tee -a ${res_file} 
+
+#  echo export OMP_NUM_THREADS=$t ';' export KMP_PLACE_THREADS=$top ';' export KMP_AFFINITY=${aff},verbose ';' likwid-perfctr -m -c 0-$thx -g $group -- /home/hpc/unrz/unrz280/girih/build_dp/mwd_kernel --n-tests 4 --nx $N --ny $N --nz $N --target-ts $ts --target-kernel $k --nt $nt --use-omp-stat-sched --thread-group-size $tgs --cache-size $cs | tee -a ${res_file} 
+
+}
+
---
+++ scripts/mic/spt_blk_thread_scaling.sh
@@ -0,0 +1,54 @@
+#!/bin/sh 
+
+. /home/hpc/unrz/unrz280/girih/scripts/mic/run_test.sh
+
+k=1
+N=768
+ts=0
+
+# 1 thread per core tests
+res_dir=$HOME/girih/results/mic_thread_scaling/
+mkdir -p $res_dir
+
+for blk_size in 1 2 4
+do
+  for group in MEM1 MEM2 MEM3 MEM4 MEM5 CPI
+  do
+
+    t=`expr $blk_size \* 6`
+    te=`expr $blk_size \* 60`
+    ccs=`expr 256 / $blk_size`
+
+    while [ $t -le $te ]
+    do
+      if [ $ts -eq 0 ]
+      then
+        cs=`expr $t \* $ccs`
+        tgs=0
+        mwd_t=0
+      else
+        cs=64
+        tgs=$blk_size
+        mwd_t=2
+      fi
+
+      affinity="E:N:${t}:$blk_size}:4"
+      nt=`expr $t \* 1`
+      if [ $nt -gt 100 ]
+      then
+        nt=100
+      elif [ $nt -lt 4 ]
+      then 
+        nt=4
+      fi 
+
+      run_test $t $affinity $group $N $ts $k $nt $tgs $cs $mwdt
+
+      t=`expr $t + $blk_size \* 6`
+
+    done
+  done
+done
+
+
+
