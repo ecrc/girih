08b5020add89fa20403710940de38d701f5e6ff1
Tareq Malas <tareq.malas@kaust.edu.sa>
2015-01-22 16:49:04 +0100

    updated the mic shell script

--- scripts/mic/run_test.sh
+++ scripts/mic/run_test.sh
@@ -12,11 +12,11 @@ run_test() {
   cs=$9
   mwdt=$10
 
-  res_file="${res_dir}kernel${k}_tgs${tgs}_ts${ts}_th${t}_grp_${group}_affinity${aff}_mwdtype${mwdt}.txt"
+  res_file="${res_dir}kernel${k}_tgs${tgs}_ts${ts}_th${t}_grp_${group}_affinity${aff}_mwdtype${mwdt}_N${N}.txt"
 
   thx=`expr $t - 1`
 
-  echo export OMP_NUM_THREADS=$t ';'likwid-perfctr -m -C $aff -g $group -- /home/hpc/unrz/unrz280/girih/build_dp/mwd_kernel --n-tests 4 --nx $N --ny $N --nz $N --target-ts $ts --target-kernel $k --nt $nt --use-omp-stat-sched --thread-group-size $tgs --cache-size $cs --mwd-type $mwdt #| tee -a ${res_file} 
+  export OMP_NUM_THREADS=$t ;likwid-perfctr -m -C $aff -g $group -- /home/hpc/unrz/unrz280/girih/build_dp/mwd_kernel --n-tests 4 --nx $N --ny $N --nz $N --target-ts $ts --target-kernel $k --nt $nt --use-omp-stat-sched --thread-group-size $tgs --cache-size $cs --mwd-type $mwdt | tee -a ${res_file} 
 
 #  echo export OMP_NUM_THREADS=$t ';' export KMP_PLACE_THREADS=$top ';' export KMP_AFFINITY=${aff},verbose ';' likwid-perfctr -m -c 0-$thx -g $group -- /home/hpc/unrz/unrz280/girih/build_dp/mwd_kernel --n-tests 4 --nx $N --ny $N --nz $N --target-ts $ts --target-kernel $k --nt $nt --use-omp-stat-sched --thread-group-size $tgs --cache-size $cs | tee -a ${res_file} 
 
