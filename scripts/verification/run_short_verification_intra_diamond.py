9b3fe9c65d035bcdef6c15b2dae0bbf3a44c5e1c
Tareq Malas <tareq.malas@kaust.edu.sa>
2015-04-23 10:02:42 +0300

    updated the short diamond verification code

--- scripts/verification/run_short_verification_intra_diamond.py
+++ scripts/verification/run_short_verification_intra_diamond.py
@@ -4,15 +4,16 @@ def main():
     import sys
     from verification_utils import run_verification
 
-    k = int(sys.argv[1])
-
-    wf = 1
-
-    print "Verifying kernel number: %d" % k
-
     dim_list = [[16, 32,  128,1],
                 [16, 64,  32, 3]]
-    # Selected MPI topologies
+ 
+    # Different MWD approaches
+    for mwd_t in range(3):
+        for nx, ny, nz, t in dim_list:
+            for kernel in [0,1,5]:
+                run_verification(nx=nx, ny=ny, nz=nz, ts=2, kernel=kernel, t_dim=t, mwd_type=mwd_t)
+
+   # Selected MPI topologies
     top_list= [[1,1,1],
                [1,2,1]]
     for npx,npy,npz in top_list:
@@ -21,7 +22,7 @@ def main():
                 nx = nlx*npx
                 ny = nly*npy
                 nz = nlz*npz
-                run_verification(nx=nx, ny=ny, nz=nz, ts=k, npx=npx, npy=npy, npz=npz, kernel=kernel, t_dim=t, wavefronts=wf)
+                run_verification(nx=nx, ny=ny, nz=nz, ts=2, npx=npx, npy=npy, npz=npz, kernel=kernel, t_dim=t)
 
 
     dim_list = [[16, 32,  128,1],
@@ -35,7 +36,7 @@ def main():
                 nx = nlx*npx
                 ny = nly*npy
                 nz = nlz*npz
-                run_verification(nx=nx, ny=ny, nz=nz, ts=k, npx=npx, npy=npy, npz=npz, kernel=kernel, t_dim=t, dp=1, concat=1, wavefronts=wf)
+                run_verification(nx=nx, ny=ny, nz=nz, ts=2, npx=npx, npy=npy, npz=npz, kernel=kernel, t_dim=t, dp=1, concat=1)
 
     for npx,npy,npz in top_list:
         for nlx, nly, nlz, t in dim_list:
@@ -43,7 +44,7 @@ def main():
                 nx = nlx*npx
                 ny = nly*npy
                 nz = nlz*npz
-                run_verification(nx=nx, ny=ny, nz=nz, ts=k, npx=npx, npy=npy, npz=npz, kernel=kernel, t_dim=t, dp=1, wavefronts=wf)
+                run_verification(nx=nx, ny=ny, nz=nz, ts=2, npx=npx, npy=npy, npz=npz, kernel=kernel, t_dim=t, dp=1)
 
 
     for npx,npy,npz in top_list:
@@ -52,7 +53,7 @@ def main():
                 nx = nlx*npx
                 ny = nly*npy
                 nz = nlz*npz
-                run_verification(nx=nx, ny=ny, nz=nz, ts=k, npx=npx, npy=npy, npz=npz, kernel=kernel, t_dim=t, concat=1, wavefronts=wf)
+                run_verification(nx=nx, ny=ny, nz=nz, ts=2, npx=npx, npy=npy, npz=npz, kernel=kernel, t_dim=t, concat=1)
 
   
 if __name__ == "__main__":
