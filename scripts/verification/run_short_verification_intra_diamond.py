1280ce6ba3d5bfc69ed26b29bf378bddc60d46ca
Tareq Malas <tareq.malas@kaust.edu.sa>
2015-04-23 09:13:12 +0200

    updated the diamond short verification code

--- scripts/verification/run_short_verification_intra_diamond.py
+++ scripts/verification/run_short_verification_intra_diamond.py
@@ -4,16 +4,19 @@ def main():
     import sys
     from verification_utils import run_verification
 
-    dim_list = [[16, 32,  128,1],
-                [16, 64,  32, 3]]
- 
+    dim_list = [[32, 128,  128,1],
+                [32, 256,  32, 3]]
+
+    print "Verifying different wavefront paralellization stratigies" 
     # Different MWD approaches
-    for mwd_t in range(3):
+
+    for kernel in [0,1,5]:
         for nx, ny, nz, t in dim_list:
-            for kernel in [0,1,5]:
+            for mwd_t in range(3):
                 run_verification(nx=nx, ny=ny, nz=nz, ts=2, kernel=kernel, t_dim=t, mwd_type=mwd_t)
 
-   # Selected MPI topologies
+    print "Verifying MPI configurations"
+    # Selected MPI topologies
     top_list= [[1,1,1],
                [1,2,1]]
     for npx,npy,npz in top_list:
