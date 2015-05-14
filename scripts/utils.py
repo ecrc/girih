cf52da281010b08b250af6ed661ee082721a5e78
Tareq Malas <tareq.malas@kaust.edu.sa>
2015-05-14 15:30:51 +0300

    considering diamond size and nwf in the testing script

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,11 +6,11 @@ def run_test(kernel, ts, nx, ny, nz, nt, target_dir, **kwargs):
   from scripts.conf import conf
 
   job_template=Template(
-"""$set_threads$th; $mpirun_cmd $pinning_cmd $pinning_args $exec_path --n-tests $ntests --disable-source-point --npx $npx --npy $npy --npz $npz --nx $nx --ny $ny --nz $nz  --verbose $verbose --target-ts $ts --nt $nt --target-kernel $kernel --cache-size $cs --thread-group-size $tgs --mwd-type $mwdt --bsx $bsx --num-wavefronts $nwf --verify $verify | tee $outpath""")
+"""$set_threads$th; $mpirun_cmd $pinning_cmd $pinning_args $exec_path --n-tests $ntests --disable-source-point --npx $npx --npy $npy --npz $npz --nx $nx --ny $ny --nz $nz  --verbose $verbose --target-ts $ts --nt $nt --target-kernel $kernel --cache-size $cs --thread-group-size $tgs --mwd-type $mwdt --bsx $bsx --num-wavefronts $nwf --t-dim $tb  --verify $verify | tee $outpath""")
 
   # set default arguments
   defaults = {'dry_run':0, 'is_dp':1, 'tgs':1, 'cs':8192, 'mwdt':1, 'npx':1, 'npy':1, 'npz':1, 'nwf':-1,
-              'bsx':1000000, 'ntests':2, 'alignment':16, 'verify':0, 'verbose':1, 'th': 1}
+              'bsx':1000000, 'ntests':2, 'alignment':16, 'verify':0, 'verbose':1, 'th': 1, 'tb':-1}
   # set the default run commands and arguments of the machine
   defaults.update(conf.machine_conf)
 
