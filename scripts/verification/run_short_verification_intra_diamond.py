019e2ab63428fde43b7490dd4775700708b45372
Tareq Malas <tareq.malas@kaust.edu.sa>
2015-06-01 09:59:09 +0300

    added verification test cases for multi-dimensional intra tile parallelizm

--- scripts/verification/run_short_verification_intra_diamond.py
+++ scripts/verification/run_short_verification_intra_diamond.py
@@ -6,11 +6,24 @@ def main():
 
     dim_list = [[32, 160,  128,1],
                 [32, 256,  64, 3]]
+    # intra tile parallelizm: (tgs, nwf, thz, thy, thx)
+    intra_tile_l = [(2,2,2,1,1), (2,2,1,2,1), (2,1,1,1,2),
+                    (4,4,4,1,1), (4,4,1,1,4), (4,2,1,2,2), (4,2,2,1,2), (4,1,1,1,4)
+                 ]
 
     print "Verifying different wavefront paralellization stratigies" 
-    # Different MWD approaches
+    for kernel in [0, 1]:
+        for nx, ny, nz, t in dim_list:
+            for mwd_t in [1, 2]:
+                for tgs,nwf,thz,thy,thx in intra_tile_l:
+                    print "mwd_type:(tgs, nwf, thz, thy, thx) ",mwd_t, tgs,nwf,thz,thy,thx
+                    run_verification(nx=nx, ny=ny, nz=nz, ts=2, kernel=kernel, t_dim=t, mwd_type=mwd_t, tgs=tgs, num_threads=8, nwf=nwf, thx=thx, thy=thy, thz=thz)
 
-    for kernel in [6, 0, 1, 5]:
+
+    dim_list = [[32, 160,  128,1],
+                [32, 256,  64, 3]]
+    print "Verifying different wavefront paralellization stratigies" 
+    for kernel in [0, 1, 5, 6]:
         for nx, ny, nz, t in dim_list:
             for mwd_t in [0, 1]:
                 print "mwd_type: ",mwd_t
--- scripts/verification/verification_utils.py
+++ scripts/verification/verification_utils.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
-def run_verification(nx, ny, nz, ts, t_dim=3, npx=1, npy=1, npz=1, kernel=0, dp=0, concat=0, num_threads=0, tgs=1, nwf=-1, bs_x=1000000, mwd_type=1):
+def run_verification(nx, ny, nz, ts, t_dim=3, npx=1, npy=1, npz=1, kernel=0, dp=0, concat=0, num_threads=0, tgs=1, nwf=-1, bs_x=1000000, mwd_type=1, thx=1, thy=1, thz=1):
     import os
     import subprocess
     from string import Template
+
+    # backward compatibility:w
+    if thz==1 and thy==1 and thx==1 and tgs>1:
+      thz=tgs 
 
     # Compute the required number of time steps for the wave to propogate from to the whole domain
     stencil_radius=[4,1,1,1,4,1,1] # IMPORTANT: this list has to match the stencil kernels list of the code
@@ -15,12 +19,12 @@ def run_verification(nx, ny, nz, ts, t_dim=3, npx=1, npy=1, npz=1, kernel=0, dp=
     
     binary=binary + "/mwd_kernel"
 
-    if(nwf == -1): nwf = 4*tgs
+    if(nwf == -1): nwf = 4*thz
 
-    cmd_template=Template('mpirun_rrze -block -np $np $binary --nt $nt --npx $npx --npy $npy --npz $npz --nx $nx --ny $ny --nz $nz --verbose 0 --verify 1 --target-ts $ts --target-kernel $kernel --t-dim $t_dim --halo-concatenate $concat --thread-group-size $tgs --num-wavefronts $nwf --bsx $bs_x --mwd-type $mwd_type')
+    cmd_template=Template('mpirun -np $np $binary --nt $nt --npx $npx --npy $npy --npz $npz --nx $nx --ny $ny --nz $nz --verbose 0 --verify 1 --target-ts $ts --target-kernel $kernel --t-dim $t_dim --halo-concatenate $concat --thread-group-size $tgs --num-wavefronts $nwf --bsx $bs_x --mwd-type $mwd_type --thx $thx --thy $thy --thz $thz')
 
     np = npx*npy*npz
-    cmd_str = cmd_template.substitute(np=np, nt=nt, npx=npx, npy=npy, npz=npz, nx=nx, ny=ny, nz=nz, t_dim=t_dim, ts=ts, kernel=kernel, binary=binary, concat=concat, tgs=tgs, nwf=nwf, bs_x=bs_x, mwd_type=mwd_type)
+    cmd_str = cmd_template.substitute(np=np, nt=nt, npx=npx, npy=npy, npz=npz, nx=nx, ny=ny, nz=nz, t_dim=t_dim, ts=ts, kernel=kernel, binary=binary, concat=concat, tgs=tgs, nwf=nwf, bs_x=bs_x, mwd_type=mwd_type, thx=thx, thy=thy, thz=thz)
     if num_threads == 0:
         num_threads = max(1, min(8,16/np))
     omp_threads = "export OMP_NUM_THREADS=%d" % num_threads
