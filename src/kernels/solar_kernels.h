f72d3726d9fc70eef8d91ffbc3687441d9604981
Tareq Malas <tareq.malas@kaust.edu.sa>
2015-09-02 13:04:26 +0300

    fixed the terminology of the solar kernels and added naive one for 1WD implementation

--- src/kernels/solar_kernels.h
+++ src/kernels/solar_kernels.h
@@ -1,7 +1,8 @@
 #include "solar_spt_blk.ic"
 #include "solar_spt_blk_stat_sched.ic"
 
-#include "solar_par_components.ic" // Contains the basic computations for the MWD solvers
+#include "solar_naive.ic" // For 1WD implementation
+#include "solar_par_components.ic" // OpenMP parallel componets
 #include "solar_1wf.ic"
 //#include "solar_mwf.ic"
 #include "solar_femwf.ic"
