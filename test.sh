e910eca7a438d9c460b8656ba115188c858f1e28
kadircs <kadir.cs@gmail.com>
2017-06-05 10:59:01 +0300

    Hatem told me how to run girih for performance tests. Girih auto-tunes number of threads working on a single tile

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 make -j  
 #gs=512;  nt=8;    verify=1; num_threads=32;  tgs=4   ntests=1; thz=2
+gs=512;  nt=8;    verify=1; num_threads=12;  tgs=4   ntests=1; thz=2
 #gs=512; nt=2101; verify=0; num_threads=18;  tgs=6;  ntests=1; thz=3        #wave
-gs=512; nt=500;  verify=0; num_threads="4 8 16 32"; tgs=4; ntests=2; thz=2 #eage paper
+#gs=512; nt=500;  verify=0; num_threads="4 8 16 32"; tgs=4; ntests=2; thz=2 #eage paper
 
 #gs=512; nt=2101; verify=0; num_threads=12; tgs=6
 #gs=512; nt=10; verify=1; num_threads=12; tgs=6
@@ -15,8 +16,11 @@ dir="."
 #dir="../ogirih"
 for nthread in $num_threads;do
     export OMP_NUM_THREADS=$nthread
+    #export OMP_NUM_THREADS=
+    echo OMP: $OMP_NUM_THREADS
     #DIAMOND
-    cmd=$dir"/build/mwd_kernel --nx $gs  --ny $gs --nz $gs --nt $nt --mwd-type 1 --target-ts 2 --verify $verify $source  --npx 1 --npy 1 --npz 1 --thread-group-size $tgs --thx 1 --thy 2 --thz $thz --threads $nthread --n-tests $ntests"
+    cmd=$dir"/build/mwd_kernel --nx $gs  --ny $gs --nz $gs --nt $nt --mwd-type 1 --target-ts 2 --verify $verify $source  --npx 1 --npy 1 --npz 1   --n-tests $ntests  --threads $nthread "
+    #cmd=$dir"/build/mwd_kernel --nx $gs  --ny $gs --nz $gs --nt $nt --mwd-type 1 --target-ts 2 --verify $verify $source  --npx 1 --npy 1 --npz 1 --thread-group-size $tgs --thx 1 --thy 2 --thz $thz --threads $nthread --n-tests $ntests"
     #SPATIAL
     #cmd="./build/mwd_kernel --nx $gs  --ny $gs --nz $gs --nt $nt --target-kernel 0 --mwd-type 0 --target-ts 0 --verify 1 $source"
     echo $cmd
