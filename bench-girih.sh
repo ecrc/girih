ab0b18879cb0ed54b2dde2f0e4d06cea7be5a900
kadir@jasmine <kadir.cs@gmail.com>
2017-06-12 11:14:33 +0300

    Small edit

--- bench-girih.sh
+++ bench-girih.sh
@@ -27,7 +27,7 @@ fi
 
 
 gs=512; nt=2101;verify=0;ntests=1  #exawave
-#gs=512; nt=506; num_threads="1 2 4 8 12 16 20 24 28 32";verify=0;ntests=2   #eage paper
+gs=512; nt=506; num_threads="1 2 4 8 12 16 20 24 28 32";verify=0;ntests=1   #eage paper
 #gs=512; nt=506; num_threads="20 24 28 32";verify=0;ntests=2   #eage paper
 nexp=22
 #shaheen     0 1 2 3 4 5  6  7  8  9  10 11 12 13 14  15 16 17 18 19 20 21 22
