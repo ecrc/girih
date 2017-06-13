3d26bc4c5e3b122a2ca0c0ffac4a59b146ae156f
kadircs <kadir.cs@gmail.com>
2017-06-13 06:06:16 +0300

    Trying to find --threads issue

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 make -j  
 #gs=512;  nt=8;    verify=1; num_threads=32;  tgs=4   ntests=1; thz=2
-gs=512;  nt=8;    verify=1; num_threads=12;  tgs=4   ntests=1; thz=2      #develop&verify
+gs=512;  nt=8;    verify=1; num_threads=32;  tgs=4   ntests=1; thz=2      #develop&verify
 #gs=512; nt=2101; verify=0; num_threads=12;  tgs=6;  ntests=1; thz=3        #wave
 #gs=512; nt=500;  verify=0; num_threads="4 8 16 32"; tgs=4; ntests=2; thz=2 #eage paper
 #gs=512; nt=500;  verify=0; num_threads="1 2 4 8 12 16 20 24 28 32"; tgs=N/A; ntests=2; thz=N/A #eage paper
@@ -22,7 +22,8 @@ for nthread in $num_threads;do
     #DIAMOND autotuning thread group size
     #cmd=$dir"/build/mwd_kernel --nx $gs  --ny $gs --nz $gs --nt $nt --mwd-type 1 --target-ts 2 --verify $verify $source  --npx 1 --npy 1 --npz 1   --n-tests $ntests  --threads $nthread "
     #DIAMOND 
-    cmd=$dir"/build/mwd_kernel --nx $gs  --ny $gs --nz $gs --nt $nt --mwd-type 1 --target-ts 2 --verify $verify $source  --npx 1 --npy 1 --npz 1 --thread-group-size $tgs --thx 1 --thy 2 --thz $thz --threads $nthread --n-tests $ntests"
+    cmd=$dir"/build/mwd_kernel --nx $gs  --ny $gs --nz $gs --nt $nt --mwd-type 1 --target-ts 2  $source  --npx 1 --npy 1 --npz 1 --thread-group-size $tgs --thx 1 --thy 2 --thz $thz  --n-tests $ntests --threads $nthread "
+    #cmd=$dir"/build/mwd_kernel --nx $gs  --ny $gs --nz $gs --nt $nt --mwd-type 1 --target-ts 2 --verify $verify $source  --npx 1 --npy 1 --npz 1 --thread-group-size $tgs --thx 1 --thy 2 --thz $thz --threads $nthread --n-tests $ntests"
     #SPATIAL
     #cmd="./build/mwd_kernel --nx $gs  --ny $gs --nz $gs --nt $nt --target-kernel 0 --mwd-type 0 --target-ts 0 --verify 1 $source"
     echo $cmd
